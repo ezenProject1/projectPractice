3dd1d5dcd38c7fa55be77c6088cf4fba0568e8be
kimmustard <kimustard44@gmail.com>
2023-11-07 20:47:19 +0900

    추가 내용2

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -4,7 +4,16 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println(1+1);
-
+		
+		
+		
+	}
+	
+	public void test1() {
+		
+	}
+	public void test2() {
+		
 	}
 
 }
