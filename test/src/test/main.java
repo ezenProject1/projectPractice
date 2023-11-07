985f7514ff698e6cc68ad19c064532a9fbff545c
kimmustard <kimustard44@gmail.com>
2023-11-07 21:15:51 +0900

    변경사항2

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -5,7 +5,11 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println(1+1);
 		System.out.println(2+2);
+		System.out.println(5+5);
 		
+		
+	}
+	public void test66() {
 		
 	}
 	
