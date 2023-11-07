ed0a65f9dca615181c86af9488a406f4725d12c7
kimmustard <kimustard44@gmail.com>
2023-11-07 21:23:18 +0900

    마지막변경1

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -3,18 +3,10 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println(1+1);
-		System.out.println(2+2);
-
-		System.out.println(1283*50);
-
-		System.out.println(5+5);
-
+	
 		
 	}
-	public void test66() {
-		
-	}
+
 	
 	public void test1() {
 		
@@ -29,13 +21,8 @@ public class main {
 	public void test4() {
 		
 	}
-	public void test6() {
+	public void test5() {
 		
 	}
-	public void test7() {
-		
-	}
-	public void test578325421() {
-		
-	}
+	
 }
